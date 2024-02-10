9c3ef3010ed3020cef89fcec869fa51249dc67f8
Yulia Starchenko <imay@amake.ru>
2024-02-11 00:01:50 +0300

    feat: started with base layout for root pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import Navbar from '@/components/shared/Navbar/Navbar';
+
+interface BaseLayoutProps {
+  children: ReactNode;
+}
+
+function BaseLayout(props: BaseLayoutProps) {
+  const { children } = props;
+
+  return (
+    <div className="background-light850_dark100 relative">
+      <Navbar />
+      <div className="flex">
+        Left Sidebar
+        <main className="flex min-h-screen flex-1 flex-col pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </main>
+        Right Sidebar
+      </div>
+      Toaster
+    </div>
+  );
+}
+
+export default BaseLayout;
