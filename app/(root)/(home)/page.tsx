dc8248be934be8e6f97f1ffdf32c79aa8f2a01c1
Yulia Starchenko <imay@amake.ru>
2024-02-17 22:19:59 +0300

    feat: updated search and filter markup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import {
+  QuestionsFilter,
+  QuestionsSearch,
+} from '@/components/features/QuestionsSearch/ui';
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="h1-bold">Home</h1>
-    </div>
+    <section>
+      <div className="mb-8 flex w-full flex-wrap justify-between gap-4 max-sm:flex-col-reverse sm:items-center">
+        <h1 className="h1-bold max-sm:w-full">All Questions</h1>
+        <Button asChild type={undefined}>
+          <Link
+            className="primary-gradient paragraph-medium ml-auto min-h-[46px] px-7 py-3 text-light-900"
+            href="/ask-question"
+          >
+            Ask a Question
+          </Link>
+        </Button>
+      </div>
+      <div className="flex gap-7 max-sm:flex-col sm:items-stretch md:flex-col">
+        <QuestionsSearch />
+        <QuestionsFilter />
+      </div>
+    </section>
   );
 };
 
--- components/features/QuestionsSearch/ui/QuestionsFilter/QuestionsFilter.tsx
+++ components/features/QuestionsSearch/ui/QuestionsFilter/QuestionsFilter.tsx
@@ -3,20 +3,25 @@ import React from 'react';
 import { QUESTIONS_FILTERS } from '@/constants';
 import { CustomSelect } from '@/components/shared/CustomSelect';
 import { ButtonsSelect } from '@/components/shared/ButtonsSelect';
+import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 
 export const QuestionsFilter = () => {
-  return (
-    <>
+  const isMobile = useMediaQuery({ width: 'md', direction: 'max' });
+
+  if (isMobile)
+    return (
       <CustomSelect
         title="Select a Filter"
         items={QUESTIONS_FILTERS}
         onSelect={(value) => console.log(value)}
       />
-      <ButtonsSelect
-        selected="newest"
-        items={QUESTIONS_FILTERS}
-        onSelect={(value) => console.log(value)}
-      />
-    </>
+    );
+
+  return (
+    <ButtonsSelect
+      selected="newest"
+      items={QUESTIONS_FILTERS}
+      onSelect={(value) => console.log(value)}
+    />
   );
 };
