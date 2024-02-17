62afaba45dc56944ac2d642aeb18a6059f259ba6
Yulia Starchenko <imay@amake.ru>
2024-02-17 19:21:57 +0300

    build: updated nextConfig - added workaround for Tailwind dark mode resolution with CSS Modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+function isCssLoader(loader) {
+  return Boolean(typeof loader?.loader === 'string' &&
+    /(?<!post)css-loader/.test(loader?.loader));
+}
+
+function hasGetLocalIdent(loader) {
+  return !!loader?.options?.modules?.getLocalIdent;
+}
+
+const nextConfig = {
+  webpack: (config) => {
+    // Workaround for Tailwind dark mode resolution with CSS Modules
+    const rules = config.module.rules.find((r) => !!r.oneOf);
+
+    rules.oneOf.forEach((loaders) => {
+      if (Array.isArray(loaders.use)) {
+        loaders.use.forEach((loader) => {
+
+          if (!(isCssLoader(loader) && hasGetLocalIdent(loader))) return config;
+
+          const {getLocalIdent} = loader.options.modules;
+
+          if (getLocalIdent) {
+            loader.options.modules.getLocalIdent = (...args) => {
+              if (args.includes('dark')) return 'dark';
+              return getLocalIdent(...args);
+            }
+          }
+        });
+      }
+    })
+    return config;
+  },
+};
 
 export default nextConfig;
